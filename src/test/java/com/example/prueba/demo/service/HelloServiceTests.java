214440a106fafba894395f6b636df02e4f30a1dd
Emanuel Gonzalez <eg554848@gmail.coml>
2026-07-12 19:35:43 -0600

    test337

--- src/test/java/com/example/prueba/demo/service/HelloServiceTests.java
+++ src/test/java/com/example/prueba/demo/service/HelloServiceTests.java
@@ -21,7 +21,7 @@ class HelloServiceTests {
     @DisplayName("Should return the correct greeting message")
     void testGetMessageReturnsCorrectMessage() {
         // Arrange
-        String expectedMessage = "Hello World from Spring Boot test36";
+        String expectedMessage = "Hello World from Spring Boot test37";
 
         // Act
         String actualMessage = helloService.getMessage();
